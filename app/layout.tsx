8ab42b9c59c6c86340458cca90afffcec53749e3
Rengga Aditya <6651354+whiterun@users.noreply.github.com>
2026-05-21 16:16:25 +0700

    Add metadata for layout and dashboard pages, including titles for Dashboard, Customers, and Invoices

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme",
+    default: "Acme",
+  },
+  description: "The official Next.js Learn Course Dashboard",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
